e4c4523138a24c8d53c45397a86e2e40b87407bf
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-30 14:06:51 +0200

    Dummy

--- django_dyn_dt/apps.py
+++ django_dyn_dt/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DynDatatablesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.dyn_datatables'
+    name = 'django_dyn_dt'
